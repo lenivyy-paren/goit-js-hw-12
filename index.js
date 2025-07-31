e52ce9cf257f7cfc09508315ea30e089dfe4f237
lenivyy-paren <vasiapypkin67@gmail.com>
2025-07-31 15:33:35 +0000

    Deploying to gh-pages from @ lenivyy-paren/goit-js-hw-12@56644c30ff34ee86a08b6fe665b00edc33f5df82 🚀

--- index.js
+++ index.js
@@ -1,24 +1,24 @@
-import{a as d,S as f,i as a}from"./assets/vendor-BK_rxH-O.js";(function(){const i=document.createElement("link").relList;if(i&&i.supports&&i.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))l(e);new MutationObserver(e=>{for(const t of e)if(t.type==="childList")for(const s of t.addedNodes)s.tagName==="LINK"&&s.rel==="modulepreload"&&l(s)}).observe(document,{childList:!0,subtree:!0});function r(e){const t={};return e.integrity&&(t.integrity=e.integrity),e.referrerPolicy&&(t.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?t.credentials="include":e.crossOrigin==="anonymous"?t.credentials="omit":t.credentials="same-origin",t}function l(e){if(e.ep)return;e.ep=!0;const t=r(e);fetch(e.href,t)}})();function g(o){return d.get("https://vercel-api-proxy-six-fawn.vercel.app/api/proxy",{params:{source:"pixabay",q:o,image_type:"photo",orientation:"horizontal",safesearch:!0}}).then(r=>r.data).catch(r=>(console.error("Error while fetching images:",r),null))}const c=document.querySelector(".loader"),u=document.querySelector(".gallery"),h=new f(".gallery a",{captionsData:"alt",captionDelay:250,captionPosition:"bottom"});function y(o){const i=o.map(({webformatURL:r,largeImageURL:l,tags:e,likes:t,views:s,comments:m,downloads:p})=>`<li class="gallery-item">
-              <a class="gallery-link" href=${l}>
-                <img src=${r} alt="${e}" width="360" height="200"/>
+import{a as q,S,i as n}from"./assets/vendor-BK_rxH-O.js";(function(){const t=document.createElement("link").relList;if(t&&t.supports&&t.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))i(e);new MutationObserver(e=>{for(const r of e)if(r.type==="childList")for(const l of r.addedNodes)l.tagName==="LINK"&&l.rel==="modulepreload"&&i(l)}).observe(document,{childList:!0,subtree:!0});function s(e){const r={};return e.integrity&&(r.integrity=e.integrity),e.referrerPolicy&&(r.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?r.credentials="include":e.crossOrigin==="anonymous"?r.credentials="omit":r.credentials="same-origin",r}function i(e){if(e.ep)return;e.ep=!0;const r=s(e);fetch(e.href,r)}})();async function p(o,t=1){const s="https://vercel-api-proxy-six-fawn.vercel.app/api/proxy",i={source:"pixabay",q:o,image_type:"photo",orientation:"horizontal",safesearch:!0,per_page:15,page:t};return(await q.get(s,{params:i})).data}const g=document.querySelector(".loader"),f=document.querySelector(".gallery"),h=document.querySelector(".load-more"),M=new S(".gallery a",{captionsData:"alt",captionDelay:250,captionPosition:"bottom"});function y(o){const t=o.map(({webformatURL:s,largeImageURL:i,tags:e,likes:r,views:l,comments:w,downloads:x})=>`<li class="gallery-item">
+              <a class="gallery-link" href=${i}>
+                <img src=${s} alt="${e}" width="360" height="200"/>
                 <div class="markup-image">
                   <ul>
                     <li class="markup-image-text">likes</li>
-                    <li class="markup-image-value">${t}</li>      
+                    <li class="markup-image-value">${r}</li>      
                   </ul>
                   <ul>
                     <li class="markup-image-text">views</li>
-                    <li class="markup-image-value">${s}</li>      
+                    <li class="markup-image-value">${l}</li>      
                   </ul>
                   <ul>
                     <li class="markup-image-text">comments</li>
-                    <li class="markup-image-value">${m}</li>      
+                    <li class="markup-image-value">${w}</li>      
                   </ul>
                   <ul>
                     <li class="markup-image-text">downloads</li>
-                    <li class="markup-image-value">${p}</li>      
+                    <li class="markup-image-value">${x}</li>      
                   </ul>
                 </div>
               </a>
-            </li>`).join("");u.innerHTML=i,h.refresh()}function v(){u.innerHTML=""}function w(){c.classList.add("visible")}function L(){c.classList.remove("visible")}const n=document.querySelector(".form");n.addEventListener("submit",async function(o){o.preventDefault();const i=o.target.elements["search-text"].value.trim();if(i===""){a.show({message:"Form field must be filled in!",titleColor:"red",position:"topRight"});return}w(),v();try{const l=(await g(i)).hits||[];l.length>0?y(l):a.show({message:"Try another way",titleColor:"red",position:"topRight"})}catch(r){a.show({message:"Something went wrong. Please check your connection or try again later.",titleColor:"red",position:"topRight"}),console.error("Fetch error:",r)}finally{L(),n.reset()}});
+            </li>`).join("");f.insertAdjacentHTML("beforeend",t),M.refresh()}function $(){f.innerHTML=""}function v(){g.classList.add("visible")}function L(){g.classList.remove("visible")}function b(){h.classList.remove("hidden")}function u(){h.classList.add("hidden")}const m=document.querySelector(".form"),k=document.querySelector(".load-more");let c="",a=1,d=0;m.addEventListener("submit",async o=>{if(o.preventDefault(),c=o.target.elements["search-text"].value.trim(),a=1,c===""){n.error({message:"Form field must be filled in!",position:"topRight"});return}$(),u(),v();try{const t=await p(c,a);if(t.hits.length===0){n.warning({message:"No images found. Try again.",position:"topRight"});return}y(t.hits),d=Math.ceil(t.totalHits/15),a<d&&b()}catch{n.error({message:"Connection error. Try later.",position:"topRight"})}finally{L(),m.reset()}});k.addEventListener("click",async()=>{a+=1,v(),u();try{const o=await p(c,a);y(o.hits),a>=d?(u(),n.info({message:"We're sorry, but you've reached the end of search results.",position:"topRight"})):b();const{height:t}=document.querySelector(".gallery-item").getBoundingClientRect();window.scrollBy({top:t*2,behavior:"smooth"})}catch{n.error({message:"Failed to load more images.",position:"topRight"})}finally{L()}});
 //# sourceMappingURL=index.js.map
